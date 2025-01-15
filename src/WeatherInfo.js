18d0675d16b2e40a71e10b40940fb228911e64d7
h-schroeder-coder <hankorschroeder@gmail.com>
2025-01-14 19:04:05 -0600

    adding convert formulas for metric to imperial

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./WeatherInfo.css";
+import WeatherCurrentTemperature from "./WeatherCurrentTemperature";
+import WeatherCurrentFeel from "./WeatherCurrentFeel";
 
 export default function WeatherInfo(props) {
     return (
-      <div className="Weatherinfo">    
+      <div className="Weatherinfo">
         <h1>
           <span id="cityName">{props.data.city}</span>,{" "}
           <span className="countryName">{props.data.country}</span>
@@ -13,21 +15,20 @@ export default function WeatherInfo(props) {
           <li className="text-capitalize">{props.data.description}</li>
         </ul>
         <div className="row">
-          <div className="col-6">
-            <img
-              src={props.data.iconUrl}
-              alt={props.data.description}
-              className="currentWeatherIcon"
-            />
-            <span className="temperature">{Math.round(props.data.temperature)}</span>
-            <span className="unit">°F</span>
-          </div>
-          <div className="col-6 weatherDetails">
+            <div className="col-7">
+                <div className="col-6">
+                    <img
+                    src={props.data.iconUrl}
+                    alt={props.data.description}
+                    className="currentWeatherIcon"
+                    />
+                </div>
+                <WeatherCurrentTemperature fahrenheit={props.data.temperature} />
+            </div>
+          <div className="col-5 weatherDetails">
             <ul>
               <li>
-                Feels Like:{" "}
-                <span className="feelsLike">{Math.round(props.data.feelsLike)}</span>
-                <span className="feelsLikeUnit">°F</span>
+                <WeatherCurrentFeel fahrenheit={props.data.feelsLike} />
               </li>
               <li>Humidity: {props.data.humidity}%</li>
               <li>Wind: {Math.round(props.data.wind)} mph</li>
